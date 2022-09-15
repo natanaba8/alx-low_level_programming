d1efe4009f8d31787dd8c631f72d498a90e26187
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 <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>
2022-09-15 01:35:18 -0900

    chenm

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,10 @@
 
 void print_alphabet(void)
 {
-	int j;
-    
+    int j;
     for (j = 'a' ; j <= 'z'; j++)
     {
         _putchar(j);
-    }
-    _putchar('\n');
-}
+        }
+        _putchar('\n');
+        }
