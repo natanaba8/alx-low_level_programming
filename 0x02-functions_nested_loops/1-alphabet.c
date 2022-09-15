9072fecc6266844012ff84ac45a9fb0648e27795
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 <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>
2022-09-15 01:41:04 -0900

    hdgfs

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,10 @@
  */
 void print_alphabet(void)
 {
-    int j;
-
-    for (j = 'a' ; j <= 'z'; j++)
+int j;
+for (j = 'a' ; j <= 'z'; j++)
     {
         _putchar(j);
-        }
+    }
         _putchar('\n');
-        }
+}
