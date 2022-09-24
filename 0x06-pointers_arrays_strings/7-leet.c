437e02d6836e7d2bb2a537f1c730a0c7dc2750fc
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 <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>
2022-09-23 18:11:58 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,20 +4,16 @@
  * *@str:string that will be encoded
  * *Return:returns encoded string
   */
-
 char *leet(char *str)
 {
-		int index1 = 0, index2;
-			char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
-
-				while (str[++index1])
-						{
-									for (index2 = 0; index2 <= 7; index2++)
-												{
-																if (str[index1] == leet[index2] ||
-																					 str[index1] - 32 == leet[index2])
-																					str[index1] = index2 + '0';
-																		}
-										}
-					return (str);
+int index1 = 0, index2;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+while (str[++index1])
+{
+for (index2 = 0; index2 <= 7; index2++)
+{
+if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2]) str[index1] = index2 + '0';
+}
+}
+return (str);
 }
