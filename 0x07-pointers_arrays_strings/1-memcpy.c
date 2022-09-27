72653ff48f890cd9df53331d84e5fffd6859d938
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 <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>
2022-09-27 15:57:16 -0700

    task1edit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *  * _memcpy - irontnoux2
- *   * @dest: pointer to char params
- *    * @src: pointer to char params
- *     * @n: size
- *      * Return: *dest
-        */
+ ** _memcpy - irontnoux2
+ ** @dest: pointer to char params
+ ** @src: pointer to char params
+ ** @n: size
+ ** Return: *dest
+  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
