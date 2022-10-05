7af7481cf1abdb907bb8bc3983c1026b0cbbd379
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 <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>
2022-10-04 17:08:52 -0700

    strcat

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *_strcat - concatenates  the string pointed to by @src to
  * the end of the string pointed to by @dest
@@ -8,17 +7,12 @@
  *
  * Return: returns poiner to @dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
