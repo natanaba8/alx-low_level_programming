89023d8df791098ecbcae71b96541340599b6174
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 <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>
2022-10-04 18:22:03 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,21 +7,17 @@
  * *@argv: array of arguments
  * *Return: returns 0
   */
-
 int main(int argc, char *argv[])
 {
-		int i, j, mul;
-
-			if (argc <= 2)
-					{
-	printf("Error\n");
-	return (1);
+int i, j, mul;
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
 }
-
-		i = atoi(argv[1]);
-		j = atoi(argv[2]);
-		mul = i * j;
-
-		printf("%d\n", mul);
-		return (0);
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+mul = i * j;
+printf("%d\n", mul);
+return (0);
 }
