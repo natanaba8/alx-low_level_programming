8db31d5818da626663ee926881b23f63c39fabac
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 <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>
2022-10-04 18:17:03 -0700

    task1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * *main - prints the minimum number of coins to make change
  * * for an amount of money
@@ -8,42 +7,40 @@
  * *@argv: array of arguments
   *Return: returns 1 if there is an error; else returns 0
   */
-
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
-
-	if (argc != 2)
-	{
-			printf("Error\n");
-				return (1);
-	}
-	cents = atoi(argv[1]);
-	while (cents > 0)
-	{
-			coins++;
-				if ((cents - 25) >= 0)
+int cents, coins = 0;
+if (argc != 2)
 {
-	cents -= 25;
-	continue;
+printf("Error\n");
+return (1);
 }
-	if ((cents - 10) >= 0)
-	{
-	cents -= 10;
-	continue;
-	}
-	if ((cents - 5) >= 0)
+cents = atoi(argv[1]);
+while (cents > 0)
 {
-	cents -= 5;
-	continue;
-}
-		if ((cents - 2) >= 0)
+coins++;
+if ((cents - 25) >= 0)
 {
-	cents -= 2;
-	continue;
+cents -= 25;
+continue;
 }
-	cents--;
-	}
-	printf("%d\n", coins);
-	return (0);
+if ((cents - 10) >= 0)
+{
+cents -= 10;
+continue;
+}
+if ((cents - 5) >= 0)
+{
+cents -= 5;
+continue;
+}
+if ((cents - 2) >= 0)
+{
+cents -= 2;
+continue;
+}
+cents--;
+}
+printf("%d\n", coins);
+return (0);
 }
