9d467de2f603acb1edd2569b201740a07e72d2f2
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 <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>
2022-10-05 15:20:16 -0700

    task6bones

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
  * *strtow - splits a stirng into words
  * *@str: string to be splitted
  * *
  * *Return: pointer to the array of splitted words
  * */
-
 char **strtow(char *str)
 {
-	char **split;
-	int i, j = 0, temp = 0, size = 0, words = num_words(str);
-	if (words == 0)
-	return (NULL);
-	split = (char **) malloc(sizeof(char *) * (words + 1));
-	if (split != NULL)
-	{
+char **split;
+int i, j = 0, temp = 0, size = 0, words = num_words(str);
+if (words == 0)
+return (NULL);
+split = (char **) malloc(sizeof(char *) * (words + 1));
+if (split != NULL)
+{
 for (i = 0; i <= len(str) && words; i++)
 {
 if ((str[i] != ' ') && (str[i] != '\0'))
