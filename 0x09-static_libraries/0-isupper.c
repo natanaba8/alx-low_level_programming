be7f6449c2f19ed03ff31335d8161304e32340cf
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 <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>
2022-10-04 17:02:13 -0700

    isupper

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -4,11 +4,9 @@
 *@c: tested character
 *Return: returns 1 if it is uppercase , 0 if not
 */
-
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-		return (1);
-
-	return (0);
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+return (0);
 }
