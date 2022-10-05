ed0f15044cb4d284592741b0a7161dad38c012c8
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 <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>
2022-10-05 15:14:22 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,11 +7,10 @@
  * *Return: NULL in case of failure , but pointer to new string in
  * *case of success
   */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
-	int index, concat_index = 0,  len = 0;
+char *concat_str;
+int index, concat_index = 0,  len = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
