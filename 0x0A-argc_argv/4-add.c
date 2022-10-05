79491d732e47b1cea5f9aea32a8752ae34b58fff
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 <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>
2022-10-04 18:30:40 -0700

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * *main - adds positive numbers
  * *@argc: number of arguments
  * *@argv: array of arguments
  * *Return: 0 on success, 1 on failure
   */
-
 int main(int argc, char *argv[])
 {
-		int i, j, sum = 0;
-
-	for (i = 1; i < argc; i++)
+int i, j, sum = 0;
+for (i = 1; i < argc; i++)
 {
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
+for (j = 0; argv[i][j] != '\0'; j++)
 {
-	printf("Error\n");
-	return (1);
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
 }
-	}
-		sum += atoi(argv[i]);	
 }
-	printf("%d\n", sum);
-	return (0);
+sum += atoi(argv[i]);	
+}
+printf("%d\n", sum);
+return (0);
 }
