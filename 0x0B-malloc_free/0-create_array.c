82d210ed971d7f5ff8e22608bb8e1d055b2904bb
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 <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>
2022-10-05 15:11:42 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,10 +7,9 @@
  * *Return: NULL if size is zero or if it fails,
  * *pointer to array if everything is normal.
   */
-
 char *create_array(unsigned int size, char c)
 {
-		char *array;
+char *array;
 unsigned int index;
 if (size == 0)
 return (NULL);
