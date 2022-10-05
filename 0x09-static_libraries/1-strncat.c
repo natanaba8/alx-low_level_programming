1feaf74f7cd06e9d845831563faa8c9cbc0a45c9
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 <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>
2022-10-04 17:14:06 -0700

    strncat

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -6,17 +6,12 @@
  *@n:integer parameter to compare index to
  *Return: returns new concatenated string
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
