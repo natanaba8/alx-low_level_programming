27631cfc1c598f51354a037ff142522de6acf703
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 <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>
2022-10-04 17:05:15 -0700

    memset

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -8,13 +8,12 @@
 *@n:number of byte
 *Return: returns new value of target
 */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
-	{
-		s[n - 1] = b;
-		n--;
-	}
-	return (s);
+while (n)
+{
+s[n - 1] = b;
+n--;
+}
+return (s);
 }
