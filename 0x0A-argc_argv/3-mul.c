74730bbbdc35de49939f30ac7ef369a79209019d
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 <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>
2022-10-04 18:44:09 -0700

    task3edede

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,14 +10,15 @@
 int main(int argc, char *argv[])
 {
 int i, j, mul;
-if(argc <= 2)
+if( argc <= 2 )
 {
 printf("Error\n");
 return (1);
 }
 i = atoi(argv[1]);
 j = atoi(argv[2]);
-mul = i * j;
+mul = i*j;
 printf("%d\n", mul);
 return (0);
 }
+
