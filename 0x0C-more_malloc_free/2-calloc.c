43ec94b3341d94a97961ac1e81a72e219eb672df
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 <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>
2022-10-07 16:18:21 -0700

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,8 +11,8 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-		void *p;
-		unsigned int i;
+void *p;
+unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 p = malloc(nmemb * size);
