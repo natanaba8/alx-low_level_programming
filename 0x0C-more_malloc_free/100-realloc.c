10591c9545368ce689567f493fda854fd69e9814
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 <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>
2022-10-07 16:38:25 -0700

    task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,8 +11,8 @@
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-		char *nptr;
-		unsigned int i;
+char *nptr;
+unsigned int i;
 if (new_size == old_size)
 return (ptr);
 if ((new_size == 0) && (ptr != NULL))
