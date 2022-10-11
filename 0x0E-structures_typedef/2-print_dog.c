5222f4c4ccd624cd7cb83a432d2b2227c39a061c
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 <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>
2022-10-11 15:52:03 -0700

    task2edit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
 /**
  ** print_dog - Prints all the data of a dog
  ** @d: A dog structure
@@ -10,7 +9,7 @@
   */
 void print_dog(struct dog *d)
 {
-		if (d != NULL)
+if (d != NULL)
 {
 printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 printf("Age: %f\n", (d->age) ? d->age : 0);
