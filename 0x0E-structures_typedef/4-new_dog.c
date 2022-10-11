89f0d42349af921f78fa6495be5125dac24b12e7
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 <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>
2022-10-11 15:55:09 -0700

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ char *_strcpy(char *dest, char *src);
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-		dog_t *cutie_dog;
+dog_t *cutie_dog;
 int name_l = 0, own_l = 0;
 if (name != NULL && owner != NULL)
 {
