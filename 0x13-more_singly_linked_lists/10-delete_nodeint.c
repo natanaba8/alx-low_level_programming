26461007a495dcbcb5f866371ffab34f7501846c
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 <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>
2022-10-26 15:42:37 -0700

    task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
   */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *old_node = NULL;
+listint_t *old_node = NULL;
 listint_t *previous_node = NULL;
 unsigned int i = 0, list_len = listint_len(*head);
 if ((index > list_len) || (list_len == 0))
