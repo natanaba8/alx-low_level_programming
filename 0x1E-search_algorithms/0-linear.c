da32ea614a3760db76ba3c42fa58e3e62abb5491
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 <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>
2023-04-12 14:57:20 -0700

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
  * linear_search - searches for a value in a sorted array of integers
  * @array: array of integers
@@ -7,19 +6,16 @@
  * @value: value to search for
  * Return: index of value or -1 if not found
  */
-
-
 int linear_search(int *array, size_t size, int value)
 {
-	size_t inc;
-
-	if (array == NULL)
-		return (-1);
-	for (inc = 0; inc < size; inc++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
-		if (array[inc] == value)
-			return (inc);
-	}
-	return (-1);
+size_t inc;
+if (array == NULL)
+return (-1);
+for (inc = 0; inc < size; inc++)
+{
+printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
+if (array[inc] == value)
+return (inc);
+}
+return (-1);
 }
